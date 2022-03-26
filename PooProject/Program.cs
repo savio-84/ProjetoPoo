433466e367cb223d4c3d5b9fce29855ca169fe75
savio-84 <62775935+savio-84@users.noreply.github.com>
2022-03-26 11:42:18 -0300

    Adicionar arquivos de projeto.

---
+++ PooProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PooProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
